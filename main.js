ff9b6d7c2b576e5381c7022f8f9c8de46da712b4
Parmeshwar Kotnur <76037757+KotnurPJ@users.noreply.github.com>
2021-07-23 00:17:24 +0530

    Added more checks

--- main.js
+++ main.js
@@ -1,40 +1,37 @@
-//Calculate Tip
-function calculateTip() {
-  var billAmt = document.getElementById("billamt").value;
-  var serviceQual = document.getElementById("serviceQual").value;
-  var numOfPeople = document.getElementById("peopleamt").value;
+$(document).ready(function(){
 
-  //validate input
-  if (billAmt === "" || serviceQual == 0) {
-    alert("Please enter values");
-    return;
-  }
-  //Check to see if this input is empty or less than or equal to 1
-  if (numOfPeople === "" || numOfPeople <= 1) {
-    numOfPeople = 1;
-    document.getElementById("each").style.display = "none";
-  } else {
-    document.getElementById("each").style.display = "block";
-  }
+    var output = "";
 
-  //Calculate tip
-  var total = (billAmt * serviceQual) / numOfPeople;
-  //round to two decimal places
-  total = Math.round(total * 100) / 100;
-  //next line allows us to always have two digits after decimal point
-  total = total.toFixed(2);
-  //Display the tip
-  document.getElementById("totalTip").style.display = "block";
-  document.getElementById("tip").innerHTML = total;
+    $("#outputBox").hide();
 
-}
+    $("#calculate").on("click", function(){
+        var bill = $("#bill").val();
+        var currency = $("#currency").find(":selected").val();
+        var serviceQuality = $("#service").find(":selected").val();
+        var people = $("#people").val();
 
-//Hide the tip amount on load
-document.getElementById("totalTip").style.display = "none";
-document.getElementById("each").style.display = "none";
+        if(currency === "" || bill === "" || serviceQuality === "" || people === ""){
+            $("#outputBox").show();
+            $("#output").html("*All fields are required").css({"color":"#FF0000", "font-size":"1.6rem"});
+            $("sup, small").hide();
+        }else{
+            var output = ((bill * serviceQuality) / people);
+            output = Math.round(output * 100) / 100;
+            output = output.toFixed(2);
 
-//click to call function
-document.getElementById("calculate").onclick = function() {
-  calculateTip();
-
-};
+            $("#outputBox").show();
+            $("#output").html(output);
+            if(currency === "INR"){
+                $(".CurrencySymbol").html("₹");
+            }else if(currency === "USD"){
+                $(".CurrencySymbol").html("$");
+            }else if(currency === "EUR"){
+                $(".CurrencySymbol").html("€");
+            }else if(currency === "AUD"){
+                $(".CurrencySymbol").html("A$");
+            }else{
+                $(".CurrencySymbol").html("¥");
+            }
+        }
+    });
+});
